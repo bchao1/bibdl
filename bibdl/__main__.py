a0bce0397e07f545ea6859f24faba53aaa76d545
Brian Chao <whereisbrianchao520@gmail.com>
2020-01-26 23:22:57 +0800

    fix main file name

---
+++ bibdl/__main__.py
@@ -0,0 +1,21 @@
+from argparse import ArgumentParser
+from .Search import BibSearch
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('-t', '--title', type = str, nargs = '+', help = "Title of paper.")
+    parser.add_argument('--max_results', type = int, default = 10000)
+    parser.add_argument('-l', '--list', type = str, default = './list.txt')
+    parser.add_argument('--bib', type = str, default = './refs.bib')
+    args = parser.parse_args()
+
+    if args.title is not None and args.list is not None:
+        print("Choose to search by paper list or by single title.")
+    elif args.title is None and args.list is None:
+        print("Specify to search by paper list or by single title.")
+    else:
+        bibsearch = BibSearch(args.max_results)
+        if args.title:
+            bib = bibsearch.search_single(' '.join(args.title))
+        elif args.list:
+            bibs = bibsearch.search_from_file(args.list, args.bib)
